7061d8cd8a71050b6f717133eaef3a8b64424e68
Jordan Caldwell <caldwelljordan40@gmail.com>
2018-10-26 23:15:20 -0400

    Did some work with Entity and Attribute

--- ERModeltoRelationalModel/src/Core/Attribute.java
+++ ERModeltoRelationalModel/src/Core/Attribute.java
@@ -14,13 +14,57 @@ import java.util.ArrayList;
  */
 public class Attribute {
     
-    String name;
-    AttrType type;
-    ArrayList<Attribute> comp; //simple attribute components that make up a composite attribute
+    private String name;
+    private AttrType type;
+    private int level;
+    private String indent;
+    private ArrayList<Attribute> comp; //simple attribute components that make up a composite attribute
     
-    public void Attribute(String name, AttrType type) {
+    public Attribute(String name, AttrType type, int level) {
         this.name = name; //name of the attribute
         this.type = type; //type of the attribute
+        comp = new ArrayList<>(); //initialize a composite's sub-attributes if needed
+        this.level = level;
+        for (int i = 0; i < level+1; i++)
+            indent += " ";
     }//constructor
+    
+    //alterType: change the type of the attribute if needed
+    public void alterType(AttrType newType) { type = newType; }
+    
+    //alterName: change the name if desired
+    public void alterName(String newName) { name = newName; }
+    
+    //addCompAttr: add an attribute to a composite's parts
+    public void addCompAttr(String cname, AttrType ctype) {
+        if (type != AttrType.COMPOSITE) {
+            System.err.println("Cannot add branching attributes - Not a COMPOSITE attribute");
+            return;
+        }
+        Attribute a = new Attribute(cname, ctype, level + 1);
+        comp.add(a);
+    }//addCompAttr
+    
+    //print information to a string
+    @Override
+    public String toString() {
+        String ret = name + " (" + type.toString() + ")";
+        for (Attribute a : comp) {
+            ret = ret + "\n" + indent + " ->" + a.toString();
+        }
+        return ret;
+    }//toString
+
+    public String getName() {
+        return name;
+    }
+
+    public AttrType getType() {
+        return type;
+    }
+
+    public ArrayList<Attribute> getComp() {
+        return comp;
+    }
     
 }
--- ERModeltoRelationalModel/src/Core/Entity.java
+++ ERModeltoRelationalModel/src/Core/Entity.java
@@ -5,6 +5,8 @@
  */
 package Core;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author caldw
@@ -12,5 +14,59 @@ package Core;
  *         it can be regular or weak (with an identifying relationship)
  */
 public class Entity {
+    
+    private String name;
+    private EntityType type;
+    private Relationship idRel; //identifying relationship
+    private ArrayList<Attribute> attr;
+    
+    public Entity(String name, EntityType type) {
+        this.name = name;
+        this.type = type;
+        attr = new ArrayList<>();
+    }//constructor(for strong)
+    
+    public Entity(String name, EntityType type, Relationship idRel) {
+        this.name = name;
+        this.type = type;
+        attr = new ArrayList<>();
+        if (type != EntityType.WEAK)
+            return;
+        this.idRel = idRel;
+    }//constructor(for weak)
+    
+    //addAttr: add an attribute to this Entity
+    public void addAttr(String name, AttrType type) {
+        Attribute a = new Attribute(name, type, 0);
+        attr.add(a);
+    }//addAttr
+    
+    //addAttrToComp: add a sub-attribute to a composite attribute
+    public void addAttrToComp(String parAttr, String cname, AttrType ctype) {
+        for (Attribute a : attr) {
+            if (a.getName().equals(parAttr) && a.getType() == AttrType.COMPOSITE) {
+                a.addCompAttr(cname, ctype);
+                return;
+            } else if (a.getType() == AttrType.COMPOSITE) {
+                Attribute b = findAttr(parAttr, a.getComp());
+                if (b.getName().equals(parAttr)) { 
+                    b.addCompAttr(cname, ctype);
+                    return;
+                }
+            }//if
+        }//for
+    }//addAttrToComp
+    
+    public Attribute findAttr(String name, ArrayList<Attribute> attrs) {
+        Attribute attr = new Attribute(" ", AttrType.SIMPLE, -1);
+        for (Attribute a : attrs) {
+            if (a.getName().equals(name) && a.getType() == AttrType.COMPOSITE) {
+                return a;
+            } else if (a.getType() == AttrType.COMPOSITE) {
+                attr = findAttr(name, a.getComp());
+            }//if
+        }//for
+        return attr;
+    }
     
 }
