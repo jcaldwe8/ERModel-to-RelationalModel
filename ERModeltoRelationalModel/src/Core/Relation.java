b575664258d909ab00a07fdc01b6eb88205e8810
Jordan Caldwell <caldwelljordan40@gmail.com>
2018-11-12 12:02:03 -0500

    Adding some functionality to Relation

--- ERModeltoRelationalModel/src/Core/Relation.java
+++ ERModeltoRelationalModel/src/Core/Relation.java
@@ -6,6 +6,8 @@
 package Core;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -15,5 +17,40 @@ public class Relation {
     
     ArrayList<RAttribute> attr;
     ArrayList<FunctDep> F;
+    Set<String> key;
+    
+    public Relation() {
+        attr = new ArrayList<>();
+        F = new ArrayList<>();
+        key = new HashSet<>();
+    }//Constructor
+    
+    public void addAttr(String a) {
+        attr.add(new RAttribute(a));
+    }//addAttr
+    
+    public void addAttr(String a, String relRef, String attrRef) {
+        attr.add(new RAttribute(a, new ForeignKey(relRef, attrRef)));
+    }//addAttr
+    
+    //setKeyAttr: set the key attributes of this entity type
+    public void setKeyAttr(String... keys) {
+        key.clear();
+        boolean in;
+        for (String a : keys) {
+            in = false;
+            for (RAttribute at : attr) {
+                if (at.getName().equals(a)) {
+                    in = true;
+                    break;
+                }//if
+            }
+            if (in) {
+                key.add(a);
+            } else {
+                System.err.println("Attribute " + a + " is not currently in the array of attributes!!!\nAdd it before declaring it part of the key for the entity!!!");
+            }//if-else
+        }//for-each
+    }//setKeyAttr
     
 }
